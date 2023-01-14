195e0c32b784056d836750e741ea25feb773980d
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-01-14 22:11:17 +0800

    feat: create ast

---
+++ create-ast.js
@@ -0,0 +1,43 @@
+const babel = require("@babel/core");
+
+const code2 = 'function greet(input) {return input ?? "Hello world";}';
+result = babel.transformSync(code2, { ast: true });
+
+console.log(result.ast);
+
+/*
+Node {
+  type: 'File',
+  start: 0,
+  end: 54,
+  loc: SourceLocation {
+    start: Position { line: 1, column: 0, index: 0 },
+    end: Position { line: 1, column: 54, index: 54 },
+    filename: undefined,
+    identifierName: undefined
+  },
+  errors: [],
+  program: Node {
+    type: 'Program',
+    start: 0,
+    end: 54,
+    loc: SourceLocation {
+      start: [Position],
+      end: [Position],
+      filename: undefined,
+      identifierName: undefined
+    },
+    sourceType: 'module',
+    interpreter: null,
+    body: [ [Node] ],
+    directives: [],
+    leadingComments: undefined,
+    innerComments: undefined,
+    trailingComments: undefined
+  },
+  comments: [],
+  leadingComments: undefined,
+  innerComments: undefined,
+  trailingComments: undefined
+}
+*/

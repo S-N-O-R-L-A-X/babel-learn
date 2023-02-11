ea1d46755d55a7acef75ebc4278e594c801b4fec
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-02-11 15:01:29 +0800

    feat: add inset-before-if

---
+++ change-node-in-ast/insert-before-if.js
@@ -0,0 +1,40 @@
+const babelParser = require('@babel/parser');
+const traverse = require("@babel/traverse");
+const types = require('@babel/types');
+const generator = require('@babel/generator');
+const template = require('@babel/template')
+
+
+// 定义try/catch语句模板
+let objTemplate = `
+obj.left=LEFT;
+obj.right=RIGHT;
+`;
+
+// 创建模板
+const temp = template.default(objTemplate);
+
+const code = `function square(n) {
+  if(n>2)
+    return n * n;
+}`;
+const outer = "const obj={};\n" + code;
+
+const ast = babelParser.parse(outer);
+traverse.default(ast, {
+
+    IfStatement(path) {
+        // 给模版增加key，添加console.log打印信息
+        let tempArgumentObj = {
+            LEFT: types.identifier(path.node.test.left.name || path.node.test.left.value),
+            RIGHT: types.numericLiteral(path.node.test.right.name || path.node.test.right.value),
+        };
+
+        // 通过temp创建try语句的AST节点
+        const newNode = temp(tempArgumentObj);
+
+        path.insertBefore(newNode);
+    }
+});
+
+console.log(generator.default(ast).code);

6895d9b15b1c52056c2d2c599ab95751cd46a597
S-N-O-R-L-A-X <S-N-O-R-L-A-X@users.noreply.github.com>
2023-02-21 22:49:32 +0800

    feat: init get-params

--- get-info-from-ast/get-params.js
+++ get-info-from-ast/get-params.js
@@ -1,51 +1,41 @@
 const babelParser = require('@babel/parser');
 const traverse = require("@babel/traverse");
 const types = require('@babel/types');
-const template = require('@babel/template');
-const generator = require('@babel/generator');
+const template = require('@babel/template')
+
+const get_branches = [];
 
 function square(n) {
-    const obj = {};
     if (n > 2)
-        n = n + 1;
-    if (n < 4) {
         return n * n;
-    }
 };
 
 // 定义try/catch语句模板
 let objTemplate = `
-obj[IDX]={};
-obj[IDX].left=LEFT;
-obj[IDX].right=RIGHT;
-obj[IDX].len=LEFT.length;
+obj.left=LEFT;
+obj.right=RIGHT;
 `;
 
 // 创建模板
 const temp = template.default(objTemplate);
 
-let cnt = 0;
+
 const ast = babelParser.parse(square.toString());
 traverse.default(ast, {
     IfStatement(path) {
+        // 给模版增加key，添加console.log打印信息
+        /* istanbul ignore next */
+        get_branches.push(path.node);
         let tempArguments = {
-            IDX: String(cnt),
             LEFT: types.identifier(path.node.test.left.name || path.node.test.left.value),
             RIGHT: types.NumericLiteral(path.node.test.right.name || path.node.test.right.value),
         };
 
-        ++cnt;
         // 通过temp创建所需语句的AST节点
         const newNode = temp(tempArguments);
 
-        // const newNode = template.default(`obj.left=${types.identifier(path.node.test.left.name || path.node.test.left.value)};
-        // obj.right==${types.identifier(path.node.test.left.name || path.node.test.left.value)};`)
         path.insertBefore(newNode);
     }
 });
 
-const f = generator.default(ast).code;
-console.log(f);
-
-const modified_function = eval("(false || " + f + ")");
-console.log(modified_function(3));
+console.log(get_branches);
